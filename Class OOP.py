73a2d5544deb550d40ecad756bb10d3657530281
Ademir Gomes <dmrademir@gmail.com>
2021-02-25 10:32:05 -0300

    Class Variables

--- Class OOP.py
+++ Class OOP.py
@@ -1,19 +1,36 @@
 # Python Object-Oriented Programing
+# Class Variables
+
 
 class Employee:
+    nums_of_emps = 0
+    raise_amount = 1.04
+
     def __init__(self, first, second, pay):
         self.first = first
         self.second = second
         self.pay = pay
         self.email = f"{self.first}.{self.second}@company.com"
 
+        Employee.nums_of_emps += 1
+
     def fullname(self):
         return f"{self.first} {self.second}"
 
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_amount)
+
 
 emp_1 = Employee('Carlos', 'Garcia', 60000)
-emp_2 = Employee('Melannie', 'Perez', 40000)
+emp_2 = Employee('Melannie', 'Perez', 70000)
+emp_3 = Employee('Carla','Martinez', 78000)
+emp_2.raise_amount = 1.05
+print(emp_2.__dict__)
 
-print(emp_1.email)
-print(emp_1.fullname())
-print(Empregado.fullname(emp_1))
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+print(Employee.nums_of_emps)
+
+# print(Employee.__dict__)
