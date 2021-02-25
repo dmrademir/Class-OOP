8aec11854d1d8959507189c3228a9a8060d03e4e
Ademir Gomes <dmrademir@gmail.com>
2021-02-25 11:29:59 -0300

    Subclasses

--- Class OOP.py
+++ Class OOP.py
@@ -1,18 +1,15 @@
 # Python Object-Oriented Programing
-# classmethods and staticmethods
+# Subclasses
 
 
 class Employee:
-    nums_of_emps = 0
-    raise_amount = 1.04
+        raise_amount = 1.04
 
     def __init__(self, first, second, pay):
         self.first = first
         self.second = second
         self.pay = pay
         self.email = f"{self.first}.{self.second}@company.com"
-
-        Employee.nums_of_emps += 1
 
     def fullname(self):
         return f"{self.first} {self.second}"
@@ -21,26 +18,7 @@ class Employee:
         self.pay = int(self.pay * Employee.raise_amount)
 
 
-    @classmethod
-    def set_raise_amt(cls,amount):
-        cls.raise_amount = amount
 
-    @classmethod
-    def from_string(cls,emp_str):
-        first, second, pay = emp_str.split('-')
-        return cls(first, second, pay)
+dev_1 = Employee('Carlos', 'Garcia', 60000)
+dev_2 = Employee('Melannie', 'Perez', 70000)
 
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
-
-
-emp_1 = Employee('Carlos', 'Garcia', 60000)
-emp_2 = Employee('Melannie', 'Perez', 70000)
-
-import datetime
-my_date = datetime.date(2021,2,25)
-
-print(emp_1.is_workday(my_date))
